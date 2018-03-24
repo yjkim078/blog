e14c6a62bec3167bb3bdf36649b3580a03c91c41
Kim Yeonju <37463710+yjkim078@users.noreply.github.com>
2018-03-24 10:32:02 +0900

    Add files via upload

---
+++ IsCycleNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class IsCycleNumber {
+
+	public static void main(String[] args) {
+		
+		int number;
+		int quotient ;
+		int remainder ;
+		int result=0;
+		int count=0;
+	
+		Scanner scanner = new Scanner(System.in);
+		int inputNumber = scanner.nextInt();
+
+		if(inputNumber>0 && inputNumber<=99) { //입력받은 수가 0보다 크고 99이하일 경우에 실행
+			number=inputNumber;
+			
+			quotient = number/10;
+			remainder=number -(quotient*10);
+			
+			while(result !=inputNumber) {
+				
+				result=result % 10;
+				result=quotient+remainder;
+				quotient=remainder;
+				remainder=result;
+				count++;
+				System.out.printf("싸이클 수는 %d",count);
+			}
+			
+		}
+		else 
+		{
+			System.out.printf("범위를 벗어났습니다.");
+		}
+	}
+
+}
